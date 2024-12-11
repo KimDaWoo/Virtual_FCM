6b5b3e87f7649491ab62c9cf65cb25ede1faec85
KimDaWoo <dawoo0000@naver.com>
2024-12-11 14:29:11 +0900

    Fix server error handling

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors'; // cors 패키지 임포트
 import notificationRoutes from './routes/notification.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// CORS 설정
+app.use(cors()); // 모든 출처에서의 요청 허용
 
 // Middleware
 app.use(bodyParser.json());
